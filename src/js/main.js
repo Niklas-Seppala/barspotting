85e62d4b8b81e8dfe5e6e1a322ad5bc97ac809ea
Niklas Seppälä <niklas.t.seppala@gmail.com>
2020-09-17 16:58:30 +0300

    creates data module

--- src/js/main.js
+++ src/js/main.js
@@ -2,17 +2,23 @@
 
 import { map } from "./map.js";
 import { ui } from "./ui.js"
+import { locationAPI, routesAPI } from "./data.js";
 
 window.onload = () => {
+    const fetchStaticData = [locationAPI.getNightlifeAsync(), locationAPI.getPizzaAsync()];
     ui.init();
     map.create('map');
     map.instance.on('locationfound', events.onLocationFound);
     map.instance.on('locationerror', events.onLocationError);
+
+    Promise.all(fetchStaticData).then(data => {
+        const [bars, pizzas] = data;
+        alert('dataaaa')
+    });
 }
 
 
 export const events = {
-
     onLocationFound: function(position) {
         map.user.position = position.latlng;
         map.refreshUserLocationMarker();
