3a8139b14e3cf9b56659b9008ed944a393bf11e0
Niklas Seppälä <niklas.t.seppala@gmail.com>
2020-09-17 11:50:56 +0300

    creates settings slider

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,24 @@ import { map } from "./map.js";
 
 window.onload = () => {
 
+    const btn = document.querySelector('#settings-bar-button');
+    const settingsBar = document.querySelector('#settings-bar');
+    console.log(btn);
+    btn.addEventListener('click', _ => {
+        if (!settingsBar.classList.contains('anim-panel-down') &&
+            !settingsBar.classList.contains('anim-panel-up')) {
+                settingsBar.classList.add('anim-panel-down');
+                return;
+        }
+        settingsBar.classList.toggle('anim-panel-down');
+        settingsBar.classList.toggle('anim-panel-up');
+    })
+
+
     map.create('map');
     map.instance.on('locationfound', onLocationFound);
     map.instance.on('locationerror', onLocationError);
+
 }
 
 function onLocationFound(position) {
